3553597f4ebefbb6a4e599aa45b90931b3e7afb4
JCF <jcf@Arwen.local>
2007-08-22 13:35:20 +0200

    * Controller, Layout fur Kapitel 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,16 @@ class PagesController < ApplicationController
   def index
     @pages = Page.find :all
   end
-  
+
   def show
     @page = Page.find(params[:id])
+  rescue
+    render :file => 'public/404.html', :status => 404
+
   end
+  
+  def list
+    @pages = Page.find :all
+  end
+  
 end
