2d7244aa3d716293cfec944a84000bd519f5446d
Jens-Christian Fischer <jcf@invisible.ch>
2008-03-20 10:06:23 +0100

    db session store

--- config/environment.rb
+++ config/environment.rb
@@ -28,7 +28,7 @@ Rails::Initializer.run do |config|
 
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
-  # config.action_controller.session_store = :active_record_store
+  config.action_controller.session_store = :active_record_store
   
   config.action_controller.session = {
     :session_key => '_referenz_session',
